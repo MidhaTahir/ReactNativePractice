2e2b7d7de0a58f43ff01d2a483470bc2506f58b0
MidhaTahir <midhatahirkhan2011@gmail.com>
2020-11-08 12:33:37 +0500

    hooks and network req

--- screens/Home.js
+++ screens/Home.js
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import { StyleSheet } from "react-native";
 import { FlatList } from "react-native-gesture-handler";
 import PalettePreview from "../components/PalettePreview";
 import  { SOLARIZED, RAINBOW, FRONTEND_MASTERS } from "./DATA";
+import axios from 'react-native-axios';
 
 const COLOR_PALETTES = [
     { paletteName: 'SOLARIZED' , colors: SOLARIZED },
@@ -11,20 +12,44 @@ const COLOR_PALETTES = [
 ]
 
 const Home = ({ navigation }) => {
+
+    const [colors, setColors] = useState([])
+    const [isRefreshing,setIsRefreshing] = useState(false);
+
+    const handlePress = useCallback(() => {
+        axios.get('https://color-palette-api.kadikraman.now.sh/palettes')
+            .then(res => setColors(res.data))
+            .catch(console.log)      
+    },[])
+
+
+    useEffect(() => {
+        handlePress();
+    }, [])
+
+
+    const handleRefresh = useCallback(()=>{
+        setIsRefreshing(true)
+        handlePress();
+        setTimeout(()=>{
+            setIsRefreshing(false)
+        },[3000])
+    },[])
+    
     return(
         <FlatList 
             style={styles.list}
-            data={COLOR_PALETTES}
+            data={colors}
             keyExtractor={item => item.paletteName}
             renderItem={({ item }) => (
                 <PalettePreview
-                    onPress={() => { navigation.navigate('ColorPalette', item); }}
+                    onPress={() => {navigation.navigate('ColorPalette', item)}}
                     palette={item}
                 />
             )}
+            refreshing={isRefreshing}
+            onRefresh={handleRefresh}
         />
-
-
     )
 }
 const styles = StyleSheet.create({
